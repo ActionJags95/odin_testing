2ceccb34161de3cda79d40bb6eed9c2bd3ed212b
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2024-10-24 23:28:36 +0530

    Simple JS program

---
+++ JS/1.js
@@ -0,0 +1,27 @@
+let myname = "Jagruth";
+console.log(myname);
+
+let surname = "Pillamari";
+console.log(surname); // assigning variable using let
+
+
+
+let age = 11;
+console.log(age);
+// values assigned using let can be changed when required
+age = 41;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+// pi = 10
+// values assigned using const cannot be changed
+console.log(pi);
+
+
+console.log((3 + 2) - 76 * (1 + 1))
+
+
+console.log(100/2*3)
+
+alert(myname)
