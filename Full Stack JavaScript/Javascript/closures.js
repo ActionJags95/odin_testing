90b11a4633e2274de15f2812ae948209404cdf67
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-04-14 02:34:25 +0530

    changed

--- Full Stack JavaScript/Javascript/closures.js
+++ Full Stack JavaScript/Javascript/closures.js
@@ -9,3 +9,6 @@ function makeAdding(firstNumber) {
 const add5 = makeAdding(5)
 
 console.log(add5(3));
+
+// add5 is a closure
+// A closure is the combination of a function and its surrounding also called as lexical environment 
--- Full Stack JavaScript/Javascript/factory_functions.js
+++ Full Stack JavaScript/Javascript/factory_functions.js
@@ -43,9 +43,9 @@ function createUser(name) {
   const discordName = "@" + name;
 
   let reputation = 0;
-  const getReputation = () => reputation;
-  const giveReputation = () => reputation++;
-  const takeReputation = () => reputation--;
+  const getReputation = () => reputation; // Gets the reputation of the player
+  const giveReputation = () => reputation++; // Increases the reputation of the player
+  const takeReputation = () => reputation--; // Decreases the reputation of the player
 
   return {getReputation, giveReputation, takeReputation};
 }
@@ -53,8 +53,8 @@ function createUser(name) {
 function createPlayer(name, level) {
   const {getReputation, giveReputation, takeReputation} = createUser(name);
 
-  const increaseLevel = () => level++;
-  const decreaseLevel = () => level--;
+  const increaseLevel = () => level++; // Increases the level of the player
+  const decreaseLevel = () => level--; // Decreases the level of the player
 
   return {name, getReputation, giveReputation, takeReputation, increaseLevel, decreaseLevel};
 }
@@ -67,3 +67,15 @@ p1.giveReputation();
 p1.giveReputation();
 p1.takeReputation();
 console.log(p1.getReputation());
+
+// IIFEs
+// Short for Immediately Invoked Function Expressions
+const calculator = (function () {
+  const add = (a,b) => a+b;
+  const sub = (a,b) => a-b;
+  const mul = (a,b) => a*b;
+  const div = (a,b) => a/b;
+  return {add, sub, mul, div};
+})();
+
+calculator.add(3,5);
