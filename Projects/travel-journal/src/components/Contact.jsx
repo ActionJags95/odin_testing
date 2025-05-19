b590f67ce280fb758028845fe1d4c06b206704e9
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-05-19 19:57:43 +0530

    ContactCard component

---
+++ Projects/travel-journal/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import './Contact.css'
+
+function Contact() {
+  return (
+    <>
+      <div className='contact-card'>
+        <div className='contact-image'>
+          <img src="/images/cat.png" alt="" />
+        </div>
+        <div className='contact-info'>
+          <h1>Mr.Whiskerson</h1>
+          <p> (212)  555-1234</p>
+          <p> mr.whiskaz@catnep.meow</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Contact;
