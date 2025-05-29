42417f0fd7680e51c4dcfa469a9413f04d6551a9
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-05-29 15:26:02 +0530

    Refactored the code to use classes

--- Projects/JS Library/JS/index.js
+++ Projects/JS Library/JS/index.js
@@ -1,90 +1,98 @@
-const myLibary = []
 const dialog = document.querySelector('#book-dialog');
 const addBtn = document.querySelector('#add-btn');
 const closeBtn = document.querySelector('#close-btn');
 const submitBtn = document.querySelector('#submit-btn')
 
 
+class Library {
+  #myLibrary = [];
 
+  constructor() {
 
-function Book(title, author, pages, read) {
-  if(!new.target) {
-    throw Error("Create only using 'new' keyword");
   }
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = crypto.randomUUID();
-};
-Book.prototype.alterRead = function() {
-  this.read = !this.read;
-};
 
-function updateLibrary() {
-  const booksArea = document.querySelector('.books-area');
-  const books = booksArea.querySelectorAll('.book');
+  getBook(id) {
+    return this.#myLibrary.find(book => book.id === id);
+  }
 
-  books.forEach(book => {
-    booksArea.removeChild(book);
-  });
-  
-  myLibary.forEach(book => {
-    // Setting the Book card item
-    const bookCard = document.createElement('div');
-    bookCard.setAttribute('id', book.id);
-    bookCard.setAttribute('class', 'book');
+  removeBook(id) {
+    const book = this.#myLibrary.find(book => book.id === id);
+    const index = this.#myLibrary.indexOf(book);
+    this.#myLibrary.splice(index, 1);
+  }
 
-    // Title of the book
-    const title = document.createElement('h1');
-    title.textContent = `${book.title}`;
+  addToLibrary() {
+    const title = document.getElementById('title').value;
+    document.getElementById('title').value = "";
 
-    // Author of the book
-    const author = document.createElement('p');
-    author.textContent = `Author: ${book.author}`;
+    const author = document.getElementById('author').value;
+    document.getElementById('author').value = ""
 
-    // No.of pages of the book
-    const pages = document.createElement('p');
-    pages.textContent = `Pages: ${book.pages}`;
+    const pages = document.getElementById('pages').value;
+    document.getElementById('pages').value = "";
 
-    // Interactive area of the book card
-    const buttonArea = document.createElement('div');
-    buttonArea.setAttribute('class', 'book-btn');
-    // Button toggling the read status of the book
-    const readBtn = document.createElement('button');
-    readBtn.setAttribute('onclick', 'changeReadStatus(this)');
-    readBtn.textContent = book.read? "Read" : "Not Read";
-    // Button to delete the book
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-    deleteBtn.setAttribute('onclick', 'deleteBook(this)')
-    // Appeding the buttons to the interactive area
-    buttonArea.appendChild(readBtn);
-    buttonArea.appendChild(deleteBtn);
+    const isRead = document.getElementById('read').checked;
+    document.getElementById('read').checked = false;
 
-    // Appending all the elements to the book card
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(buttonArea);
+    const book = new Book(title, author, pages, isRead);
+    this.#myLibrary.push(book);
+    this.updateLibrary();
 
+    // console.log(myLibary);
+  }
+
+  updateLibrary() {
     const booksArea = document.querySelector('.books-area');
-    booksArea.appendChild(bookCard);
+    const books = booksArea.querySelectorAll('.book');
 
-  });
-}
+    books.forEach(book => {
+      booksArea.removeChild(book);
+    });
+    
+    this.#myLibrary.forEach(book => {
+      // Setting the Book card item
+      const bookCard = document.createElement('div');
+      bookCard.setAttribute('id', book.id);
+      bookCard.setAttribute('class', 'book');
 
-function addToLibrary() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const isRead = document.getElementById('read').checked;
+      // Title of the book
+      const title = document.createElement('h1');
+      title.textContent = `${book.title}`;
 
-  const book = new Book(title, author, pages, isRead);
-  myLibary.push(book);
-  updateLibrary();
+      // Author of the book
+      const author = document.createElement('p');
+      author.textContent = `Author: ${book.author}`;
 
-  // console.log(myLibary);
+      // No.of pages of the book
+      const pages = document.createElement('p');
+      pages.textContent = `Pages: ${book.pages}`;
+
+      // Interactive area of the book card
+      const buttonArea = document.createElement('div');
+      buttonArea.setAttribute('class', 'book-btn');
+      // Button toggling the read status of the book
+      const readBtn = document.createElement('button');
+      readBtn.setAttribute('onclick', 'changeReadStatus(this)');
+      readBtn.textContent = book.read? "Finished" : "Not Read";
+      // Button to delete the book
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete";
+      deleteBtn.setAttribute('onclick', 'deleteBook(this)')
+      // Appeding the buttons to the interactive area
+      buttonArea.appendChild(readBtn);
+      buttonArea.appendChild(deleteBtn);
+
+      // Appending all the elements to the book card
+      bookCard.appendChild(title);
+      bookCard.appendChild(author);
+      bookCard.appendChild(pages);
+      bookCard.appendChild(buttonArea);
+
+      const booksArea = document.querySelector('.books-area');
+      booksArea.appendChild(bookCard);
+
+    });
+  }
 }
 
 function changeReadStatus(btn) {
@@ -92,19 +100,32 @@ function changeReadStatus(btn) {
   // In this case closest DOM to btn with class '.book'
   const bookId = btn.closest('.book').id;
   // console.log(bookId);
-  const book = myLibary.find(book => book.id === bookId);
+  const book = library.getBook(bookId);
   // console.log(book);
   book.alterRead();
-  btn.textContent = book.read? "Read" : "Not read";
+  btn.textContent = book.read? "Finished" : "Not read";
 }
-
 function deleteBook(btn) {
   const bookId = btn.closest('.book').id;
-  const book = myLibary.find(book => book.id === bookId);
-  const index = myLibary.indexOf(book);
-  myLibary.splice(index, 1);
-  updateLibrary();
+  library.removeBook(bookId);
+  library.updateLibrary();
 }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+  }
+  alterRead() {
+    this.read = !this.read;
+  }
+}
+
+
+const library = new Library();
 
 // Event listeners to the buttons
 addBtn.addEventListener('click', ()=>{
@@ -116,5 +137,5 @@ closeBtn.addEventListener('click', ()=>{
 submitBtn.addEventListener('click', function(event) {
   event.preventDefault();
   dialog.close();
-  addToLibrary();
+  library.addToLibrary();
 });
