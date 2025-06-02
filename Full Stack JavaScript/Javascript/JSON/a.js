db9f385162d148b952a2c28aaba08feae8ef7c20
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-06-02 23:42:35 +0530

    changes

---
+++ Full Stack JavaScript/Javascript/JSON/a.js
@@ -0,0 +1,9 @@
+const obj = {
+  name: "Jagruth",
+  age: 20,
+  gender: "Male",
+}
+
+const str = JSON.stringify(obj);
+console.log(str);
+ 
---
+++ Full Stack JavaScript/Javascript/Linting/a.js
@@ -0,0 +1,13 @@
+function getDetails() {
+  let obj = {
+    name: "Jagruth",
+    id: 1,
+    number: "9121096718",
+    section: "A"
+  }
+
+  return obj;
+}
+
+const {id} = getDetails();
+console.log(id);
