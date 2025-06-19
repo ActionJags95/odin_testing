3b9ab3eed205f109ca0205ee84262cf3e9e9d46d
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-06-19 23:23:07 +0530

    Entry points for the project

---
+++ Projects/chef-claude/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/header'
+import Main from './components/main'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export default App
