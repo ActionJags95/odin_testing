065cea5f7de350bc99fffebf0e80f94dfe397fca
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-06-19 23:23:35 +0530

    components for the project

---
+++ Projects/chef-claude/src/components/header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+
+function Header() {
+  return (
+    <>
+      <header>
+        <img src="/src/assets/images/chef-logo.png" alt="chef-image" />
+        <h1>Chef-Claude</h1>
+        <h3></h3>
+      </header>
+    </>
+  )
+}
+
+export default Header;
---
+++ Projects/chef-claude/src/components/main.jsx
@@ -0,0 +1,16 @@
+import './main.css'
+
+function Main() {
+  return (
+    <>
+      <main>
+        <form action="" className='add-ingredient-form'>
+          <input type="text" aria-label='Add ingredients' placeholder='e.g. oregano' />
+          <button>Add ingredient</button>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default Main;
