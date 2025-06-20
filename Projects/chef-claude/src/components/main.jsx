6a0554c553b61a20b10a1ecce164b9fc7b546611
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-06-20 23:26:17 +0530

    Added a list component to view ingredients

--- Projects/chef-claude/src/components/main.jsx
+++ Projects/chef-claude/src/components/main.jsx
@@ -1,16 +1,43 @@
-import './main.css'
+import "./main.css";
 
 function Main() {
+  let items = ["Chicken"];
+
+  let itemList = items.map((item, index) => {
+    return <li id={index}>{item}</li>
+  });
+
+  function printList(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const ingredient = formData.get("ingredient");
+    // console.log(ingredient);
+    items.push(ingredient);
+    console.log(itemList);
+  }
+
   return (
     <>
       <main>
-        <form action="" className='add-ingredient-form'>
-          <input type="text" aria-label='Add ingredients' placeholder='e.g. oregano' />
-          <button>Add ingredient</button>
+        <form className="add-ingredient-form" onSubmit={printList}>
+          <input
+            id="list-item"
+            type="text"
+            aria-label="Add ingredients"
+            placeholder="e.g. oregano"
+            name="ingredient"
+          />
+          <button id="submit-btn" type="submit">
+            Add ingredient
+          </button>
         </form>
+        <ul>
+          {itemList}
+        </ul>
       </main>
     </>
-  )
+  );
 }
 
 export default Main;
