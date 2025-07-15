8e4475be4fdfc793fdf2ff6238c882a4cfc4d6a3
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-16 01:01:15 +0530

    changes

--- Full Stack JavaScript/React/src/App.jsx
+++ Full Stack JavaScript/React/src/App.jsx
@@ -1,9 +1,17 @@
-function Main() {
+import "./App.css"
+import Person from "./state/person";
+
+function App() {
   return (
     <>
-      
+      <Person details={{
+        name: "Jagruth",
+        age: 20,
+        number: 89,
+        isFavorite: true
+      }}/>
     </>
   )
 }
 
-export default Main;
+export default App;
