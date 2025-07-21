1c84d74ce0c8013e34e6c1f5dcf7c539b1720aed
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-21 23:25:39 +0530

    A seconds counter using useEffect hook

---
+++ Full Stack JavaScript/React/src/side-effects/clock.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+function Clock() {
+  const [counter, updateCounter] = useState(0);
+
+  useEffect(() => {
+    // This hook takes the calculations outside the render environment
+    const interval = setInterval(() => {
+      updateCounter((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <p>{counter} seconds have passed.</p>;
+}
+
+export default Clock;
+
+// Use effect hook takes the calculations inside outside the react render calculations
+// useEffect hook takes dependencies array as another argument apart from the callback calling the calculations
+// The array is the array of dependencies, that means, whenever on of the arguments in the array updates, useEffect resets and starts from beginning
+// By default it re-renders completely, so empty array must be provided.
