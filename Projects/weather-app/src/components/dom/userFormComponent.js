9e25a14dccbe9b955b816c4a94a98e88e1c38e20
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-22 19:33:20 +0530

    Moved the Form initializer from api to dom folder

---
+++ Projects/weather-app/src/components/dom/userFormComponent.js
@@ -0,0 +1,24 @@
+import "./userFormComponent.css";
+const root = document.querySelector("#root");
+
+function setUserForm() {
+  const form = document.createElement("form");
+  form.setAttribute("id", "locationForm");
+
+  const locationInput = document.createElement("input");
+  locationInput.setAttribute("name", "location");
+  locationInput.setAttribute("id", "location");
+  locationInput.setAttribute("placeholder", "Enter the location: (eg: Hyderabad)");
+
+  const submitBtn = document.createElement("button");
+  submitBtn.setAttribute("type", "submit");
+  submitBtn.setAttribute("id", "getLocation");
+  submitBtn.textContent = "Submit";
+
+  form.appendChild(locationInput);
+  form.appendChild(submitBtn);
+
+  root.appendChild(form);
+}
+
+export default setUserForm;
