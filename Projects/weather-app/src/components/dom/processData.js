727c09c0299ace1770aec5570b4ec9ef61d2dd4d
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-25 01:58:32 +0530

    Added components and their dataflow and mounting them to DOM

--- Projects/weather-app/src/components/dom/processData.js
+++ Projects/weather-app/src/components/dom/processData.js
@@ -1,5 +1,35 @@
+import Today from "./cards/today";
+import ForeCast from "./cards/forecast";
+
 function processData(data) {
-  console.log(data);
+  const root = document.getElementById("root"); // Getting the root element
+
+  const {
+    latitude,
+    longitude,
+    resolvedAddress,
+    timezone,
+    description,
+    days,
+    alerts,
+  } = data;
+
+  const todayWeather = days[0];
+
+  const todayDiv = Today({
+    latitude,
+    longitude,
+    resolvedAddress,
+    timezone,
+    description,
+    alerts,
+    todayWeather,
+  });
+
+  const forecastDiv = ForeCast({ days });
+
+  root.appendChild(todayDiv);
+  root.appendChild(forecastDiv);
 }
 
 export default processData;
