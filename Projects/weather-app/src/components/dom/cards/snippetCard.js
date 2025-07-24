a19ec1b19254ec0e2b3aab5903614e5e80ab6644
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-25 02:00:23 +0530

    modified snippetCard inorder to hide the scroll-bar within the border

--- Projects/weather-app/src/components/dom/cards/snippetCard.js
+++ Projects/weather-app/src/components/dom/cards/snippetCard.js
@@ -1,4 +1,4 @@
-import "./snippetCard.css"
+import "./snippetCard.css";
 
 function SnippetCard({ data }) {
   console.log(data);
@@ -7,19 +7,46 @@ function SnippetCard({ data }) {
     const card = document.createElement("div");
     card.setAttribute("class", "snippetCard");
 
-    
+    const { datetime, temp, icon } = item;
+    const today = new Date();
+    if (
+      datetime.split(":").at(0) == today.getHours() ||
+      datetime.split("-").at(2) == today.getDate()
+    )
+      card.setAttribute("class", `${card.getAttribute("class")} current`);
+
+    const datetimeText = document.createElement("p");
+    datetimeText.setAttribute("class", "datetimeText");
+    datetimeText.textContent = datetime;
+
+    const weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute("class", "forecastIcon");
+    weatherIcon.src = require(`../../../assets/weather-icons/${icon}.svg`);
+
+    const tempText = document.createElement("p");
+    tempText.setAttribute("class", "tempText");
+    tempText.textContent = temp;
+
+    card.appendChild(datetimeText);
+    card.appendChild(weatherIcon);
+    card.appendChild(tempText);
 
     return card;
   });
 
   const snippetContainer = document.createElement("div");
-  snippetContainer.setAttribute("class", "snippetContainer")
+  snippetContainer.setAttribute("class", "snippetContainer");
 
-  snippetCards.forEach(card => {
+  snippetCards.forEach((card) => {
     snippetContainer.appendChild(card);
   });
 
-  return snippetContainer;
+  const weatherContainer = document.createElement("div"); // so to wrap snippetContainer in it
+  weatherContainer.setAttribute("class", "weatherContainer");
+
+  weatherContainer.appendChild(snippetContainer);
+
+  return weatherContainer;
 }
 
 export default SnippetCard;
