b50b75116719c991b0df772f6828a93b22deba32
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-31 22:38:31 +0530

    Added the compnent dependencies

--- Full Stack JavaScript/React/src/App.jsx
+++ Full Stack JavaScript/React/src/App.jsx
@@ -1,15 +1,32 @@
 import "./App.css"
 import Person from "./state/person";
+import Input from "./state/input";
+import Accordion from "./state/shareState";
+import TaskList from "./class-based/functional";
+import TaskListClass from "./class-based/class";
+import AssignFunctional from "./class-based/assignment/functional";
+import AssignClass from "./class-based/assignment/class";
+import Clock from "./side-effects/clock";
+import Appi from "./side-effects/chat-room/App";
 
 function App() {
   return (
     <>
       <Person details={{
-        name: "Jagruth",
+        name: "Jags",
         age: 20,
         number: 89,
         isFavorite: true
       }}/>
+      <br/>
+      <Input />
+      <Accordion />
+      <TaskList name={"Jags"}/>
+      <TaskListClass name={"Jags"}/>
+      <AssignFunctional />
+      <AssignClass />
+      <Clock />
+      <Appi />
     </>
   )
 }
