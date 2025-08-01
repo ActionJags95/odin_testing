5d00bcee55657506babcd9d34eb154d26029b110
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-08-01 07:32:50 +0530

    React router demo

---
+++ Full Stack JavaScript/React-Router/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello form the main page of the app!</h1>
+        <p>Here are some examples of links to other pages</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="profile">Profile page</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Full Stack JavaScript/React-Router/src/components/profile.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+  return (
+    <>
+      <div>
+        <h1>Hello form profile page</h1>
+        <p>So, how are you?</p>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
